7abbfd17f7562e301e3f751d0f60ca11c11ec0df
mikufan <mqnu000@gmail.com>
2024-11-28 10:12:16 +0800

    feat: 增加存储单位转换

---
+++ utils/sysInfo/convert.py
@@ -0,0 +1,14 @@
+def bytes_to_MiB(num):
+    return num / 1024 / 1024
+
+
+def bytes_to_GiB(num):
+    return num / 1024 / 1024 / 1024
+
+
+def GiB_to_Mib(num):
+    return num * 1024
+
+
+def MiB_to_GiB(num):
+    return num / 1024
