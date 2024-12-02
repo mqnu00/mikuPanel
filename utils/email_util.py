f82ca39d7a3e685b58028a9d958cb6d929958f57
mikufan <mqnu000@gmail.com>
2024-12-02 14:40:20 +0800

    feat: smtp 邮件发送功能

--- utils/email_util.py
+++ utils/email_util.py
@@ -4,11 +4,6 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from utils.log_util import log
 
-smtp_server = 'smtp.163.com'  # SMTP服务器地址
-smtp_port = 587  # SMTP端口号，通常是587或465
-smtp_username = 'mqnu000@163.com'  # SMTP用户名（通常是邮箱地址）
-smtp_password = 'RDebNDs8mMxyWqzE'  # SMTP密码
-
 
 # 发送邮件
 def send_email(
@@ -18,9 +13,25 @@ def send_email(
         smtp_password: str,
         subject,
         body,
-        receive_email=None,
+        receive_email: str=None,
         is_ssl=False
 ):
+
+    """
+    邮件发送 smtp。
+
+    :param smtp_server: SMTP服务器地址。
+    :param smtp_port: SMTP服务器端口号。
+    :param smtp_username: SMTP服务器用户名。
+    :param smtp_password: SMTP服务器密码。
+    :param subject: 邮件主题。
+    :param body: 邮件正文。
+    :param receive_email: 收件人邮箱地址。默认为None，表示发给自己。
+    :param is_ssl: 是否使用SSL加密连接，默认为False。
+
+    :return: 邮件是否发送成功
+    """
+
     message = MIMEMultipart()
     message['From'] = smtp_username
     message['To'] = receive_email
@@ -44,13 +55,19 @@ def send_email(
         log.info("邮件发送成功")
     except Exception as e:
         log.exception(e)
+        return False
+    return True
 
 
 if __name__ == '__main__':
+    from password import qq_email_password
+
     send_email(
         smtp_server='smtp.qq.com',
-        smtp_port=456,
+        smtp_port=465,
         smtp_username='647983952@qq.com',
-        smtp_password='wyxnsbbehildbcea',
-
+        smtp_password=qq_email_password,
+        subject="你好",
+        body="你好",
+        is_ssl=True
     )
