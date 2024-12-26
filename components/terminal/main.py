e2b877e7a9a5e07ce79be9cf57ad125c9bf2440f
mikufan <mqnu000@gmail.com>
2024-12-26 16:35:54 +0800

    feat: terminal component

--- components/terminal/main.py
+++ components/terminal/main.py
@@ -1,139 +1,111 @@
 import asyncio
-import logging
-from typing import List, Dict
+from typing import Dict
 
-from components.terminal.terminal import Terminal, SSHInfo
+from websockets.asyncio.server import ServerConnection
+
+from components.base_component import BaseComponent
+from components.terminal.terminal import Terminal
 from core.component_init import Config
 from utils.log_util import log
-from websockets.asyncio.server import ServerConnection
-import uuid
-from websockets.asyncio.server import ServerConnection
-
-config: Config
 
 
-def init():
-    global config
-    terminals: Dict[str, Terminal] = {}
+class TerminalComponent(BaseComponent):
 
-    config = Config(
-        instance={
-            "terminals": terminals
-        },
-        is_async=False
-    )
+    def __init__(self):
+        terminals: Dict[str, Terminal] = {}
 
-    return config
+        self.config = Config(
+            instance={
+                "terminals": terminals
+            },
+            is_async=False
+        )
 
+    def create_terminal(self, info, *args, **kwargs):
+        terminal = Terminal(info)
+        terminal.connect_to_terminal()
 
-def create_terminal(info, *args, **kwargs):
-    terminal = Terminal(info)
-    terminal.connect_to_terminal()
+        terminals: Dict[str, Terminal] = self.config.instance.get('terminals')
+        terminals['123'] = terminal
 
-    terminals: Dict[str, Terminal] = config.instance.get('terminals')
-    terminals['123'] = terminal
-
-
-def check_terminal(uid, *args, **kwargs) -> Terminal | bool:
-    try:
-        terminal: Terminal = config.instance.get('terminals').get(uid)
-        return terminal
-    except IndexError:
-        return False
-    except Exception:
-        return False
-
-
-def get_terminal(uid, *args, **kwargs):
-    return check_terminal(uid)
-
-
-def del_terminal(uid: str, *args, **kwargs):
-    terminal: Terminal = config.instance.get('terminals')[uid]
-    terminal.close()
-    del config.instance.get('terminals')[uid]
-
-
-def send(uid: str, msg: str, *args, **kwargs):
-    try:
-        terminal: Terminal = config.instance.get('terminals')[uid]
-    except Exception:
-        return False
-    if not terminal.send_to_terminal(msg):
-        del_terminal(uid)
-        return False
-    return True
-
-
-def recv(uid: str, *args, **kwargs):
-    while True:
+    def check_terminal(self, uid, *args, **kwargs) -> Terminal | bool:
         try:
-            terminal: Terminal = config.instance.get('terminals')[uid]
-            res = terminal.recv_from_terminal()
-            if not res:
-                del_terminal(uid)
-                break
-            yield res
+            terminal: Terminal = self.config.instance.get('terminals').get(uid)
+            return terminal
+        except IndexError:
+            return False
         except Exception:
-            log.exception("recv_error")
-            continue
-    yield False
+            return False
 
+    def get_terminal(self, uid, *args, **kwargs):
+        return self.check_terminal(uid)
 
-init()
+    def del_terminal(self, uid: str, *args, **kwargs):
+        terminal: Terminal = self.config.instance.get('terminals')[uid]
+        terminal.close()
+        del self.config.instance.get('terminals')[uid]
 
+    def send(self, uid: str, msg: str, *args, **kwargs):
+        try:
+            terminal: Terminal = self.config.instance.get('terminals')[uid]
+        except Exception:
+            return False
+        if not terminal.send_to_terminal(msg):
+            self.del_terminal(uid)
+            return False
+        return True
 
-async def test(websocket: ServerConnection):
-    from components.terminal.terminal import SSHInfo, Terminal
-    from tests import server_password
-    create_terminal(SSHInfo(
-        host=server_password.host,
-        port=server_password.port,
-        username=server_password.username,
-        password=server_password.password
-    ))
-
-    running = True
-
-    async def receive():
-
-        async for msg in websocket:
-            if not send('123', msg):
-                break
-        print("recv done")
-
-    async def send_to():
-        async def async_wrapper():
-            gen = recv('123')
-            while True:
-                try:
-                    value = await asyncio.to_thread(next, gen)
-                    yield value
-                except StopIteration as e:
-                    # 处理返回值
-                    res = e.value
+    def recv(self, uid: str, *args, **kwargs):
+        while True:
+            try:
+                terminal: Terminal = self.config.instance.get('terminals')[uid]
+                res = terminal.recv_from_terminal()
+                if not res:
+                    self.del_terminal(uid)
                     break
-                    pass
-            yield res
+                yield res
+            except Exception:
+                log.exception("recv_error")
+                continue
+        yield False
 
-        async for res in async_wrapper():
-            if res:
-                await websocket.send(res)
-            else:
-                break
-        print('send done')
-        await websocket.close()
+    async def handle(self, websocket: ServerConnection):
+        from components.terminal.terminal import SSHInfo, Terminal
+        from tests import server_password
+        self.create_terminal(SSHInfo(
+            host=server_password.host,
+            port=server_password.port,
+            username=server_password.username,
+            password=server_password.password
+        ))
 
-    await asyncio.gather(receive(), send_to())
+        async def receive():
 
+            async for msg in websocket:
+                if not self.send('123', msg):
+                    break
+            log.info("recv done")
 
-if __name__ == '__main__':
-    init()
-    from tests import server_password
+        async def send_to():
+            async def async_wrapper():
+                gen = self.recv('123')
+                while True:
+                    try:
+                        value = await asyncio.to_thread(next, gen)
+                        yield value
+                    except StopIteration as e:
+                        # 处理返回值
+                        res = e.value
+                        break
+                        pass
+                yield res
 
-    create_terminal(SSHInfo(
-        host=server_password.host,
-        port=server_password.port,
-        username=server_password.username,
-        password=server_password.password
-    ))
+            async for res in async_wrapper():
+                if res:
+                    await websocket.send(res)
+                else:
+                    break
+            log.info('send done')
+            await websocket.close()
+
+        await asyncio.gather(receive(), send_to())
