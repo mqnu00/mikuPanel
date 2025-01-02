442d62cadd5e3b3edd5e9fc8a9489c5fbcd12fca
mikufan000 <mqnu000@gmail.com>
2025-01-02 11:24:16 +0800

    fix: 增加进程数量

--- main.py
+++ main.py
@@ -7,6 +7,6 @@ from ws.server import MikuServer
 if __name__ == '__main__':
     multiprocessing.freeze_support()
     communication.init_ipc()
-    communication.init_pool(1)
+    communication.init_pool(4)
     server = MikuServer(8000)
     server.start()
