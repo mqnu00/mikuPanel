e77e6298d0d6f0e4dd90c78c9cc47c2728fa7ffe
mqnu00 <647983952@qq.com>
2025-01-11 23:11:57 +0800

    feat: init sql_engine

--- core/communication.py
+++ core/communication.py
@@ -1,10 +1,13 @@
 import asyncio
 import multiprocessing
+import threading
 import uuid
 from asyncio.queues import Queue
 from multiprocessing import Manager
 from multiprocessing.managers import SyncManager
 from multiprocessing.pool import Pool
+from sqlalchemy import create_engine, MetaData, Table, select, Engine
+from utils.log_util import setup_logger
 
 
 class Message(object):
@@ -18,6 +21,7 @@ class Message(object):
 manager: SyncManager = None
 share: dict[str, Message] | Message = {}
 process_pool: Pool = None
+sql_engine: Engine = None
 
 
 def pool_initializer():
@@ -39,20 +43,14 @@ def init_ipc(info: str = None):
         manager = multiprocessing.Manager()
 
 
-def init_sql():
-    from sqlalchemy import create_engine
-    from utils.log_util import setup_logger
+def init_sql(num: int):
 
     sql_log = setup_logger(
         name='sqlalchemy.engine',
-        log_dir=rf'D:\program\python\code\mikuPanelGrpc\logs\test123'
+        log_dir=rf'D:\program\python\code\mikuPanelGrpc\logs\sql_engine'
     )
-    sql_engine = create_engine("mysql+pymysql://root:123456@localhost/mikuserver?charset=utf8", pool_size=4)
-    with sql_engine.connect() as conn:
-        result = conn.execute("SELECT * FROM test_2")
-        for row in result:
-            print(row)
-    # sql_engine.logger = sql_log
+    global sql_engine
+    sql_engine = create_engine("mysql+pymysql://root:123456@localhost/mikuserver?charset=utf8", pool_size=num)
 
 
 if __name__ == '__main__':
