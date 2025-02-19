adf7b7569f9b3f4bd939f9f3793c31db3616feab
mikufan000 <mqnu000@gmail.com>
2025-02-19 18:27:29 +0800

    feat: Service state

--- core/service.py
+++ core/service.py
@@ -1,11 +1,94 @@
 import multiprocessing
+from enum import Enum
+from typing import ClassVar, List
+
 from rx.subject import Subject
 from rx import operators as ops
+from transitions import Machine, EventData
+from utils.log_util import log
+
+
+class ServiceState(Enum):
+    IDLE = 1
+    PENDING = 2
+    WORKING = 3
+    DONE = 4
+    CONFIG = 5
+    ERROR = 6
+
+
+class Service(object):
+    _initial_state: ClassVar[ServiceState] = ServiceState.IDLE
+    _transitions: ClassVar[List] = [
+        {
+            'trigger': 'config',
+            'source': [ServiceState.IDLE, ServiceState.PENDING, ServiceState.WORKING],
+            'dest': ServiceState.CONFIG,
+        },
+        {
+            'trigger': 'pend',
+            'source': [ServiceState.CONFIG, ServiceState.WORKING],
+            'dest': ServiceState.PENDING,
+            'conditions': 'is_config'
+        },
+        {
+            'trigger': 'work',
+            'source': ServiceState.PENDING,
+            'dest': ServiceState.WORKING
+        },
+        {
+            'trigger': 'done',
+            'source': ServiceState.WORKING,
+            'dest': ServiceState.DONE
+        },
+        {
+            'trigger': 'error',
+            'source': '*',
+            'dest': 'error',
+            'after': '_error'
+        }
+    ]
+
+    def __init__(self):
+        self.obs = Subject()
+        self.obs.subscribe(lambda msg: log.info(f'{msg}'))
+
+    def _config(self):
+        pass
+
+    def consumer(self):
+        pass
+
+    def productor(self):
+        pass
+
+    def handle(self):
+        pass
+
+    def state_change(self, event: EventData):
+        self.obs.on_next(event.state.value)
+
+    @classmethod
+    def create(cls, *args, **kwargs):
+        service = cls(*args, **kwargs)
+        machine = Machine(
+            model=service,
+            states=ServiceState,
+            transitions=cls._transitions,
+            initial=cls._initial_state,
+            send_event=True,
+            queued=True,
+            after_state_change=service.state_change,
+        )
+        # machine.on_enter_CONNECTING('_connect')
+        return service
+
 
 # 定义一个消费者函数
 def consumer(data):
     print(f"Consumer received: {data}")
     return data.upper()  # 示例处理：将字符串转为大写
+
 
 # 定义一个观察者进程
 def observer(queue):
@@ -14,6 +97,7 @@ def observer(queue):
         if data is None:  # 使用 None 作为结束信号
             break
         return data  # 将数据传递回主进程
+
 
 # 主函数
 def main():
@@ -50,6 +134,7 @@ def main():
     # 取消订阅
     subscription.dispose()
 
+
 # 定义一个生产者进程
 def producer(queue):
     for i in range(5):
@@ -57,5 +142,9 @@ def producer(queue):
         print(f"Producer sent: Message {i}")
     queue.put(None)  # 发送结束信号
 
+
 if __name__ == "__main__":
-    main()
+    # main()
+    service = Service.create()
+    service.config()
+    service.done()
