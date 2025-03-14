f554191a23b9977b961f6fa0f15762fa5632ce40
mqnu00 <647983952@qq.com>
2025-03-14 05:21:13 -0400

    fix: 修改日志输出目录

--- utils/log_util.py
+++ utils/log_util.py
@@ -76,7 +76,7 @@ def setup_logger(
 
 log = setup_logger(
     name=multiprocessing.current_process().name,
-    log_dir=rf'D:\program\python\code\mikuPanelGrpc\logs\{multiprocessing.current_process().name}'
+    log_dir=rf'/home/lzh/program/py-code/mikuPanel/logs/{multiprocessing.current_process().name}'
 )
 log.info(log.name)
 
