fc98dab0043b47316e22f2387d2160840a77f18b
mqnu00 <647983952@qq.com>
2025-03-30 11:28:15 -0400

    feat: 运行插件 配置适配

--- core/message/action/run.py
+++ core/message/action/run.py
@@ -17,9 +17,9 @@ def execute(component_name: str, share_uid: str):
         module = importlib.import_module(f'components.{component_name}.main')
         log.info(module)
         component_class = getattr(module, f'{component_name[0].upper()+component_name[1:]}Component')
-        component_instance: BaseComponent = component_class()
-        if component_instance.config.is_need_sql:
-            log.info(component_instance.config.sql_table)
+        component_instance: BaseComponent = component_class(share_uid)
+        # if component_instance.config.is_need_sql:
+        #     log.info(component_instance.config.sql_table)
             # sql_engine.create_table(
             #     package=f'components.{component_name}.{component_name}',
             #     tables=component_instance.config.sql_table
