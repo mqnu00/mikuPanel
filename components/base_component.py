cdaa4918fd046f4e43fcc59a073a88139e3555ff
mqnu00 <647983952@qq.com>
2025-03-30 11:27:56 -0400

    feat: 插件 基础配置信息 uid

--- components/base_component.py
+++ components/base_component.py
@@ -1,13 +1,21 @@
 from websockets.asyncio.server import ServerConnection
 
-from core.component_init import Config
+from core import communication
+from core.communication import Message
 
 
-class BaseComponent(object):
+class BaseComponent():
 
-    def __init__(self):
-        self.config = Config()
+    def __init__(self, uid):
+        self.uid = uid
+        self.role = 1
+        self.msg: Message = communication.share.get(uid)
+
+    def handle(self, uid):
         pass
 
-    def handle(self, share_uid):
-        pass
+    def read(self, is_sync=True):
+        return self.msg.read(role=self.role, is_sync=is_sync)
+
+    def write(self, content, is_sync=True):
+        return self.msg.write(content, role=self.role, is_sync=is_sync)
